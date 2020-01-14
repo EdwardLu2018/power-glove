faeb2f2fd40538cac849d8b1fea60e20c9665dbb
jkoch830 <44660124+jkoch830@users.noreply.github.com>
2020-01-14 13:03:13 -0500

    Add files via upload

---
+++ Power-Glove.py
@@ -0,0 +1,65 @@
+import serial, time
+
+# data = [left pinky, left ring, left middle, left index, left thumb, 
+#         right thumb, right index, right middle, right ring, right pinky,
+#         left x, left y, left z, right x, right y, right z]
+
+class Glove(object):
+    def __init__(self, data=[0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0]):
+        self.data = data
+        self.pinkyL = data[0]
+        self.ringL = data[1]
+        self.middleL = data[2]
+        self.indexL = data[3]
+        self.thumbL = data[4]
+        self.thumbR = data[5]
+        self.indexR = data[6]
+        self.middleR = data[7]
+        self.ringR = data[8]
+        self.pinkyR = data[9]
+        self.xL = data[10]
+        self.yL = data[11]
+        self.zL = data[12]
+        self.xR = data[13]
+        self.yR = data[14]
+        self.zR = data[15]
+    
+    
+    def __repr__(self):
+        #res = "Left Pinky Resistance: " + self.pinkyL
+        return str(self.data)
+        
+    
+    def update(self, data):
+        self.__init__(data)
+        
+PowerGlove = Glove()
+ArduinoSerial = serial.Serial('/dev/ttyUSB0', 9600)
+time.sleep(2)   # wait two seconds for connection to be established
+while True:
+    serial_data = ArduinoSerial.readline()
+    print(serial_data)
+    print("h")
+    
+        
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
