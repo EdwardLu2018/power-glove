ac1e20ff606e4cdf1fad16d26dff43d529205490
Edward <lued2018@gmail.com>
2020-01-14 20:38:50 -0500

    push

--- test_type/test_type.ino
+++ test_type/test_type.ino
@@ -1,26 +1,44 @@
-uint8_t buf[8] = { 0 }; /* Keyboard report buffer */
+#include <SoftwareSerial.h>
+#include "keys.h"
 
-void setup()
-{
+SoftwareSerial jetsonSerial (2, 3); // RX, TX
+
+/* Keyboard report buffer */
+uint8_t buf[8] = { 0 };
+
+void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
-  delay(200);
+  jetsonSerial.begin(9600);
 }
 
-void loop()
-{
-  delay(200);
-  
+void loop() {
+  if (jetsonSerial.available() && Serial.available()) {
+    Serial.println(jetsonSerial.read());
+  }
+}
+
+void swipeRight() {
   buf[0] = 0;
-  buf[2] = 0xE0; // control
+  buf[2] = KEY_LEFTCTRL;
   Serial.write(buf, 8);
-  delay(200);
+  delay(100);
   buf[0] = 0;
-  buf[2] = 0x50; 
+  buf[2] = KEY_LEFT; 
   Serial.write(buf, 8);
   releaseKey();
 }
-  
+
+void swipeLeft() {
+  buf[0] = 0;
+  buf[2] = KEY_LEFTCTRL; 
+  Serial.write(buf, 8);
+  delay(100);
+  buf[0] = 0;
+  buf[2] = KEY_RIGHT; 
+  Serial.write(buf, 8);
+  releaseKey();
+}
+
 void releaseKey()
 {
   buf[0] = 0;
