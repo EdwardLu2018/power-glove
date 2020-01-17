96f5296409a36dc929e89b60018060e4a8115b10
Carolyn Lee <cnl2@andrew.cmu.edu>
2020-01-17 01:36:17 -0500

    yee

--- swipeGestureFunctions.py
+++ swipeGestureFunctions.py
@@ -1,16 +1,16 @@
 def detectSwipe(orient, velx, vely, velz, serial):
      #swipe down
-    if (vely > 400): #and (orient == 0 or orient == 6) ):
+    if (velz > 1500 and vely > -50 and vely <50 and velx > -50 and velx <50 ): #and (orient == 0 or orient == 6) ):
 
         return "palm leftm DOWN"
-    elif(vely < -1500): #and (orient == 1 or orient == 7)):
+    elif(velz < -400 and vely > -50 and vely <50 and velx > -50 and velx <50 ): #and (orient == 1 or orient == 7)):
 
         return "palm right,UP "
 
 def detectLRSwipe(orient, velx, vely, velz, serial):
-    if (velz > 1500):
+    if (velz > 1500 and vely < - 900):
       
         return "palm up, RIGHT"
         
-    elif (velz<-1500):
+    elif (velz<-1500 and vely < -900 ):
         return "palm down, LEFT"
