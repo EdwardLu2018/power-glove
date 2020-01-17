37ef819899ad4033b09e8267909e0352c4df03a1
power-glove <jkoch830@gmail.com>
2020-01-17 00:32:58 -0500

    testing

--- actions.py
+++ actions.py
@@ -3,9 +3,22 @@
 #key1 is top key, bound1 is lower number
 def chr_interval(key1, key2, key3, bound1, bound2, flex, capital):
     if capital == 0:
-        key1 = key1.upper()
-        key2 = key2.upper()
-        key3 = key3.upper()
+        if key3 == ",":
+            key1 = key1.upper()
+            key2 = key2.upper()
+            key3 = "<"
+        elif key3 == ".":
+            key1 = key1.upper()
+            key2 = key2.upper()
+            key3 = ">"
+        elif key3 == "/":
+            key1 = key1.upper()
+            key2 = ":"
+            key3 = "?"
+        else:
+            key1 = key1.upper()
+            key2 = key2.upper()
+            key3 = key3.upper()
     if flex <= bound1:
         return ord(key1)
     elif bound1 < flex <= bound2:
@@ -16,15 +29,19 @@ def chr_interval(key1, key2, key3, bound1, bound2, flex, capital):
 
 
 def call_left_action(glove):
-    if glove.mode == 0:
+    #changes mode
+    if glove.indexTouch == glove.middleTouch == glove.ringTouch ==\
+            glove.pinkyTouch == 0:
+        glove.mode = (glove.mode + 1) % 3
+    elif glove.mode == 0:
         if glove.middleTouch == 0:
             return chr("i")
         elif glove.indexTouch == 0:
             pass
             #right click
     elif glove.mode == 1:       # keyboard mode
-        if glove.indexTouch == 1:
-            if glove.ringTouch == 1:   # pinky + index keys t, g, b
+        if glove.indexTouch == 0:
+            if glove.pinkyTouch == 0:   # pinky + index keys t, g, b
                 return chr_interval('t', 'g', 'b', 535, 570, 
                                     glove.index, glove.thumbTouch)
             else:
@@ -44,29 +61,36 @@ def call_left_action(glove):
         
 
 def call_right_action(glove):
-    if glove.mode == 0:
+    #changes mode
+    if glove.indexTouch == glove.middleTouch == glove.ringTouch ==\
+            glove.pinkyTouch == 0:
+        glove.mode = (glove.mode + 1) % 3
+    elif glove.mode == 0:
         if glove.indexTouch == 0:
             return chr("i")
         elif glove.middleTouch == 0:
             pass
             #right click
+
+
     elif glove.mode == 1:       # keyboard mode
-        if glove.indexTouch == 1:
-            if glove.ringTouch == 1:   # pinky + index keys t, g, b
-                return chr_interval('y', 'h', 'n', 535, 570, 
+        if glove.indexTouch == 0:
+            if glove.pinkyTouch == 0:   # pinky + index keys t, g, b
+                return chr_interval('y', 'h', 'n', 645, 685, 
                                     glove.index, glove.thumbTouch)
             else:
-                return chr_interval('u', 'j', 'm', 535, 570, 
+                return chr_interval('u', 'j', 'm', 645, 685, 
                                     glove.index, glove.thumbTouch)
         elif glove.middleTouch == 0:
-            return chr_interval('i', 'k', ',', 600, 620, 
+            return chr_interval('i', 'k', ',', 700, 745, 
                                 glove.middle, glove.thumbTouch)
         elif glove.ringTouch == 0:
-            return chr_interval('o', 'l', '.', 665, 695, 
+            return chr_interval('o', 'l', '.', 760, 767, 
                                 glove.ring, glove.thumbTouch)
         elif glove.pinkyTouch == 0:
-            return chr_interval('p', ';', '/', 620, 650, 
+            return chr_interval('p', ';', '/', 690, 700, 
                                 glove.pinky, glove.thumbTouch)
+
     elif glove.mode == 2:
         pass
     
---
+++ testSend.py
@@ -0,0 +1,39 @@
+import data
+import serial
+import time
+import actions
+right_glove = data.Data()
+
+right_serial = serial.Serial('/dev/ttyACM0', 9600)
+
+time.sleep(3)
+previous_action = None
+while True:
+    try:
+
+        #read data from serial port
+        right_serial_data = right_serial.readline().decode('utf-8')
+        right_line = right_serial_data.replace("\n\r", "")
+        right_data = list(map(int, right_line.split(' '))) + [1]
+
+        #updating glove objects
+        right_glove.update(right_data)
+    except:
+        continue
+
+    #calling respective actions
+    right_action = actions.call_right_action(right_glove)
+    if previous_action != None and previous_action == right_action:
+        time.sleep(.3)
+        previous_action = None
+        continue
+    if right_action != None:
+        print(right_action, chr(right_action)) 
+        print("Mode: ", right_glove.mode)
+
+    
+    #sends actions to arduino (prioritizes left action)
+        SendSerial = serial.Serial('/dev/ttyTHS1', 9600)
+        SendSerial.write(chr(right_action).encode())
+        previous_action = right_action
+
