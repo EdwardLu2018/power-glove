6ba3d7b919bddccddd43c23fb29e5dcbd1b3ef91
Carolyn Lee <cnl2@andrew.cmu.edu>
2020-01-17 01:14:39 -0500

    PUSH

---
+++ swipeGestureFunctions.py
@@ -0,0 +1,16 @@
+def detectSwipe(orient, velx, vely, velz, serial):
+     #swipe down
+    if (vely > 400): #and (orient == 0 or orient == 6) ):
+
+        return "palm leftm DOWN"
+    elif(vely < -1500): #and (orient == 1 or orient == 7)):
+
+        return "palm right,UP "
+
+def detectLRSwipe(orient, velx, vely, velz, serial):
+    if (velz > 1500):
+      
+        return "palm up, RIGHT"
+        
+    elif (velz<-1500):
+        return "palm down, LEFT"
--- testGestures.py
+++ testGestures.py
@@ -2,37 +2,15 @@ import serial, time
 import numpy as np
 from data import Data
 import pickle
-#import sklearn
-
-FILENAME = "left_flex_data.txt"
-
-OPEN = '0'
-FIST = '1'
-ONE = '2'
-TWO = '3'
-THREE = '4'
-FOUR = '5'
-MIDDLE = '6'
-OK = '7'
-ROCK ='8'
-NEUTRAL = '9'
-CALI = '10'
-THUMB = '11'
-GUN = '12'
-
-def str_to_list(string):
-    return [int(s) for s in string.split(' ')]
-
-def list_to_str(lst):
-    return " ".join([str(elem) for elem in lst])
-    
+from swipeGestureFunctions import *
+from utils import *
 
 
 def main():
     ser1 = serial.Serial('/dev/cu.usbmodem14301', 9600)
     #ser2 = serial.Serial('/dev/ttyACM1', 9600)
 
-    #clf = pickle.load(open('left_glove_classifier.pkl', 'rb'))
+    clf = pickle.load(open('left_glove_classifier.pkl', 'rb'))
 
     time.sleep(1)
     count = 0
@@ -68,13 +46,17 @@ def main():
             swipevert = (detectSwipe(orientation, velx_data, vely_data, velz_data, ser1) != None)
             swipeside = (detectLRSwipe(orientation, velx_data, vely_data, velz_data, ser1) != None )
             if (tempcount < count - 10):
-                print((velx_data,vely_data, velz_data, orientation, detectSwipe(orientation, velx_data, vely_data, velz_data, ser1), detectLRSwipe(orientation, velx_data, vely_data, velz_data, ser1)))
-            if (swipeside or swipevert) and not (swipeside and swipevert):
-                tempcount = count
+                #print((velx_data,vely_data, velz_data, orientation, detectSwipe(orientation, velx_data, vely_data, velz_data, ser1), detectLRSwipe(orientation, velx_data, vely_data, velz_data, ser1)))
+                if (swipeside or swipevert) and not (swipeside and swipevert):
+                    if (swipevert):
+                        print(detectSwipe(orientation, velx_data, vely_data, velz_data, ser1))
+                    else:
+                        print(detectLRSwipe(orientation, velx_data, vely_data, velz_data, ser1))
+                    tempcount = count
            
             #print(flex_data)
 
-            #sprint(clf.predict(flex_data))
+            sprint(clf.predict(flex_data))
         # time.sleep(0.25)
 
 if __name__ == '__main__':
