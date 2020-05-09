d9abe62adaa70bda3208dbac16c7fe6658fc2f8e
Edward <lued2018@gmail.com>
2020-05-08 21:20:25 -0500

    keyboard works sometimes

--- blue_pill_keyboard/src/keyboard.h
+++ blue_pill_keyboard/src/keyboard.h
@@ -1,7 +1,7 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-// id, modifiers, reserved, keys[6], leds
+// report id, modifiers, reserved, keys[6], leds
 
 #define KEY_MOD_LCTRL  0x01
 #define KEY_MOD_LSHIFT 0x02
--- blue_pill_keyboard/src/main.c
+++ blue_pill_keyboard/src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Turns an STM32F103 device into an HID keyboard and mouse
+ *
+ * Adapted from:
+ * https://satoshinm.github.io/blog/171227_stm32hid_pill_duck_scriptable_usb_hid_device_using_an_stm32_blue_pill_from_mouse_jigglers_to_rubber_duckies.html
+ */
 #include <stdlib.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
@@ -57,49 +63,54 @@ uint8_t usbd_control_buffer[128];
 
 int main(void) {
 	rcc_clock_setup_in_hsi_out_48mhz();
+	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOC);
 
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-		GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+	// trigger device re-enumeration on startup. kind of a hack
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+	gpio_clear(GPIOA, GPIO12);
+
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 	gpio_set(GPIOC, GPIO13);
+
+	for (unsigned i = 0; i < 800000; i++) __asm__("nop");
 
 	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &config,
 						 usb_strings, sizeof(usb_strings)/sizeof(char *),
 						 usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, hid_set_config);
 
-	while (1)
+	while (0 == 0)
 		usbd_poll(usbd_dev);
 }
 
+static uint16_t mouse_click(uint8_t type) {
+	uint8_t report[5] = {MOUSE_REPORT_ID, type, 0, 0, 0};
+	return usbd_ep_write_packet(usbd_dev, 0x81, report, sizeof(report));
+}
+
 static uint16_t mouse_move_x_by(uint8_t by) {
-	uint8_t buf[5] = {MOUSE_REPORT_ID, 0, by, 0, 0};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 5);
+	uint8_t report[5] = {MOUSE_REPORT_ID, 0, by, 0, 0};
+	return usbd_ep_write_packet(usbd_dev, 0x81, report, sizeof(report));
 }
 
 static uint16_t mouse_move_y_by(uint8_t by) {
-	uint8_t buf[5] = {MOUSE_REPORT_ID, 0, 0, by, 0};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 5);
-}
-
-static uint16_t mouse_click(uint8_t type) {
-	uint8_t buf[5] = {MOUSE_REPORT_ID, type, 0, 0, 0};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 5);
+	uint8_t report[5] = {MOUSE_REPORT_ID, 0, 0, by, 0};
+	return usbd_ep_write_packet(usbd_dev, 0x81, report, sizeof(report));
 }
 
 static uint16_t mouse_scroll(uint8_t by) {
-	uint8_t buf[5] = {MOUSE_REPORT_ID, 0, 0, 0, by};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 5);
+	uint8_t report[5] = {MOUSE_REPORT_ID, 0, 0, 0, by};
+	return usbd_ep_write_packet(usbd_dev, 0x81, report, sizeof(report));
 }
 
-static uint16_t keyboard_press(uint8_t character) {
-	uint8_t buf[10] = {KEYBOARD_REPORT_ID, 0, 1, character, 0, 0, 0, 0, 0, 0};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 10);
-}
-
-static uint16_t keyboard_release() {
-	uint8_t buf[10] = {KEYBOARD_REPORT_ID, 0, 1, KEY_NONE, 0, 0, 0, 0, 0, 0};
-	return usbd_ep_write_packet(usbd_dev, 0x81, buf, 10);
+static uint16_t keyboard_press(uint8_t key) {
+	uint8_t report[9] = {0};
+	report[0] = KEYBOARD_REPORT_ID;
+	report[1] = 0;
+	report[2] = 0;
+	report[3] = key;
+	return usbd_ep_write_packet(usbd_dev, 0x81, report, sizeof(report));
 }
 
 void sys_tick_handler(void) {
@@ -112,15 +123,16 @@ void sys_tick_handler(void) {
 	// 	by = -by;
 	// mouse_move_by(by);
 
-	// mouse_scroll(-5);
+	// mouse_scroll(-1); // negative is up
+	// for (int i = 0; i < 10000; ++i) __asm__("nop");
 
-	mouse_click(LEFT_CLICK);
-	for (int i = 0; i < 10000; i++) __asm__("nop");
-	mouse_click(0x0);
+	// mouse_click(LEFT_CLICK);
+	// for (int i = 0; i < 10000; ++i) __asm__("nop");
+	// mouse_click(0x0);
 
-	// keyboard_press(KEY_A);
-	// for (int i=0; i < 100000; i++);
-	// keyboard_release();
+	keyboard_press(KEY_A);
+	// keyboard_press(KEY_NONE);
+	for (int i = 0; i < 800000; ++i) __asm__("nop");
 
 	gpio_toggle(GPIOC, GPIO13);
 }
